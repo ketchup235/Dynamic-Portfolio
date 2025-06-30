0de5ce92abf9c2511584b718199bb4eb41fb2019
ktumuluri01 <kasyaptu@gmail.com>
2025-06-30 15:31:51 -0400

    add typing feature to name

--- static/script.js
+++ static/script.js
@@ -1,6 +1,17 @@
 const proj = document.getElementsByClassName("proj");
 const details= document.getElementsByClassName("details");
-
+const typed = new Typed('#typed', {
+    strings: ["Kasyap Tumuluri"],
+    startDelay: 500,
+    typeSpeed: 100,     
+    backSpeed: 50,       
+    showCursor: true,
+    cursorChar: '|',
+    loop: false,
+    onComplete (self) {
+        self.cursor.style.visibility = 'hidden'
+    }
+  });
 
 for(let i=0; i<proj.length; i++) {
     proj[i].addEventListener("click", function() {
@@ -8,3 +19,4 @@ for(let i=0; i<proj.length; i++) {
         details[i].classList.toggle("visible");
     })
 }
+
