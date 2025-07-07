c3bec0ac3ad12b5641d45ff28ae7cdb684d4e0f0
ktumuluri01 <kasyaptu@gmail.com>
2025-07-07 12:02:28 -0400

    adding db features

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask import render_template
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    db=sqlite3.connect("portfolio.db")
+    cur=db.cursor()
+    cur.execute("SELECT title, description, languages, link FROM projects")
+    projects=cur.fetchall()
+    db.close()
+    return render_template("index.html", projects=projects)
